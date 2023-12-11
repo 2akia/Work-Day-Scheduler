a51a6125d859045c90559762e0dede4df4953e05
2akia <2akssultana@gmail.com>
2023-12-11 14:06:05 +0000

    added the functin for time block

--- script.js
+++ script.js
@@ -5,9 +5,33 @@ $("#currentDay").text(today.format("dddd, MMMM DD"));
 
 let timeBlocks = document.getElementsByClassName("time-block");
 
+
 let currentHour = dayjs().hour();
 
+Array.from(timeBlocks).forEach(timeBlock => {
+    let timeBlockIdString = timeBlock.id,
+        timeBlockHour;
+    console.log(timeBlock);
 
+    if (timeBlockIdString) {
+        timeBlockHour = parseInt(timeBlockIdString.split("-")[1]);
+    }
+    if (timeBlockHour) {
+        if (currentHour === timeBlockHour) {
+            timeBlock.classList.add("present");
+        } else if (currentHour < timeBlockHour) {
+            timeBlock.classList.add("future");
+        } else if (currentHour > timeBlockHour) {
+            timeBlock.classList.add("past");
+        } else {
+            setColor(timeBlock, "future");
+        }
+    }
+});
+
+function setColor(element, color) {
+    element.style.backgroundColor = color;
+};
 
 
 
